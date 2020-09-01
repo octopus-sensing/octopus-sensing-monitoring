a8aecfefcc3270d3ac9727d0957c7bb8927fdc3e
Aidin Gharibnavaz <aidin@aidinhut.com>
2020-09-01 19:24:42 +1200

    server: Fix random data generator

--- server/octopus_sensing_data_visualizer/handlers.py
+++ server/octopus_sensing_data_visualizer/handlers.py
@@ -12,6 +12,9 @@ class ApiHandler:
 
     @cherrypy.expose
     def fetch(self):
+        # TODO:
+        # Ask for Pickle format
+        # Convert the data from list of records to one list per channel
         return json.dumps({"eeg": []})
 
 
@@ -21,8 +24,8 @@ class FakeApiHandler:
     def fetch(self):
         # Three seconds of random data
         eeg_data = []
-        for _ in range(3 * 128):
-            eeg_data.append([round(random.uniform(
-                0.01, 0.9), 5) for _ in range(16)])
+        for _ in range(16):
+            eeg_data.append([round(random.uniform(0.01, 0.9), 5)
+                             for _ in range(3 * 128)])
 
         return json.dumps({"eeg": eeg_data})
