95fcd2c6132e2f84785dc1e18d548ba4372e4307
Aidin Gharibnavaz <aidin@aidinhut.com>
2020-09-01 19:25:55 +1200

    ui: Fix ServerData type and back-end URL

--- ui/src/services.ts
+++ ui/src/services.ts
@@ -3,7 +3,7 @@ import { ServerData } from './types'
 export function fetchServerData(): Promise<ServerData> {
     // TODO: Get the URL from the current browser's URL
     return new Promise((resolve, reject) => {
-        fetch("http://127.0.0.1:8080/api/fetch")
+        fetch("http://" + window.location.host + "/api/fetch")
             .then(response => {
                 if (!response.ok) {
                     reject("Could not fetch data from the server: " + response.statusText)
--- ui/src/types.ts
+++ ui/src/types.ts
@@ -1,1 +1,1 @@
-export type ServerData = { eeg?: any; gsr?: any; ppg?: any; camera?: any; }
+export type ServerData = { eeg?: number[][]; gsr?: any; ppg?: any; camera?: any; }
