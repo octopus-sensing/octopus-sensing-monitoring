8672cdd62cd0921f7679968906de4df1555d6bac
Aidin Gharibnavaz <aidin@aidinhut.com>
2020-09-05 21:03:55 +1200

    Implement ApiHandler to communicate with actual Octopus Sensing

--- server/octopus_sensing_data_visualizer/handlers.py
+++ server/octopus_sensing_data_visualizer/handlers.py
@@ -1,5 +1,10 @@
 import random
 import json
+import pickle
+import urllib.request
+# The data received from the server might be in numpy format.
+# We need to import 'numpy' to unpickle it.
+import numpy
 
 import cherrypy
 
@@ -12,10 +17,34 @@ class ApiHandler:
 
     @cherrypy.expose
     def fetch(self):
-        # TODO:
-        # Ask for Pickle format
-        # Convert the data from list of records to one list per channel
-        return json.dumps({"eeg": []})
+        request = urllib.request.Request(
+            "http://127.0.0.1:9330", headers={"Accept": "application/pickle"}, method='GET')
+        with urllib.request.urlopen(request) as response:
+            if response.getcode() != 200:
+                raise RuntimeError(
+                    f"Could not get data from Octopus Sensing {response.status} - {response.reason}")
+            serialized_data = response.read()
+
+        raw_data = pickle.loads(serialized_data)
+        data = {}
+
+        if 'eeg' in raw_data:
+            data["eeg"] = self._restructure_eeg(raw_data["eeg"])
+
+        return json.dumps(data)
+
+    def _restructure_eeg(self, data):
+        channels = len(data[0])
+        result = []
+
+        for _ in range(channels):
+            result.append([])
+
+        for record in data:
+            for i, channel_data in enumerate(record):
+                result[i].append(channel_data)
+
+        return result
 
 
 class FakeApiHandler:
