07bff886c96072fdf54149023f1a854cee6117e5
Aidin Gharibnavaz <aidin@aidinhut.com>
2021-07-08 20:26:25 +1200

    Publish version 1.1.1 after the previous bug fix

--- build.sh
+++ build.sh
@@ -8,4 +8,11 @@ cd ../server/
 cp ../README.md .
 poetry install
 poetry build
+
+read -p "Publish to PyPi? [y/N]" -e answer
+if [ $answer == "y" -o $answer == "Y" ]
+then
+  poetry publish
+fi
+
 rm README.md
